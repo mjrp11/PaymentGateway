24934d71500e6e7c8035a664fcadeb3f00892ca1
mjr11 <mjrp11@gmail.com>
2021-01-24 16:00:38 +0000

    master: Change running on docker to powershell

--- RunningOnDocker.ps1
+++ RunningOnDocker.ps1
@@ -1,3 +1,3 @@
-dotnet publish -c release -o ./PaymentGatewayApi --no-restore .\PaymentGateway\PaymentGateway.Api\
+dotnet publish -c release -o ./PaymentGatewayApi --no-restore .\src\PaymentGateway.Api\
 docker build --rm -t paymentgatewayapi . 
 docker run -i --tty --rm -p 2345:8080 paymentgatewayapi bash
